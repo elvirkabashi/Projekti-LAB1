0b4ab5b1b47fa41c7617035e3f193d16faaaf323
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2023-04-17 01:13:49 +0200

    navbar

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
