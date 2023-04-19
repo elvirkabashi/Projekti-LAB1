d21133c4ed21aab28c6f4cd9cf3f6953bc760ae5
Blerta Jusufii <blertamicrosoft10@gmail.com>
2023-04-19 16:22:38 +0300

    Hero Component done

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
+      <Heros/>
     </div>
   );
 }
