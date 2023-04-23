1cce3f33299fa367aa03a861fd8089017c9bdd59
BleronaHaxhiu <bleronahaxhiu5@gmail.com>
2023-04-23 21:50:36 +0200

    testing a GITHUB

--- frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -4,7 +4,8 @@ export const Carousel = () => {
     return (
         <div className='container mt-5' style={{ height: 550 }}>
             <div className='homepage-carousel-title'>
-                <h3>Find your next "I stayed up too late reading" book.</h3>
+                <h3>Find your next "I stayed up too late reading" react book.</h3>
+                <h1> Hello world!</h1>
             </div>
             <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 
                 d-none d-lg-block' data-bs-interval='false'>
