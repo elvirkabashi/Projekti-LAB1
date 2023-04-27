7e846f9db416f67af3ccb473de609fe928bb4ce2
Blerta Jusufii <blertamicrosoft10@gmail.com>
2023-04-27 22:19:16 +0200

    book model and useeffect done

--- frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -1,6 +1,63 @@
 import { ReturnBook } from "./ReturnBook";
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
 
 export const Carousel = () => {
+   
+    const[books,setBooks] = useState<BookModel[]>([]);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const[httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+         const fetchBooks = async () =>{
+            const baseUrl:string = "http://localhost:8080/api/books";
+
+            const url:string =`${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url);
+
+            if(!response.ok){
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+
+            const loadedBooks: BookModel[] = [];
+
+            for(const key in responseData){
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies:responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category:responseData[key].category,
+                    img: responseData[key].img,
+
+
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+
+         };
+         fetchBooks().catch((error:any)=>{
+            setIsLoading(false);
+            setHttpError(error.message);
+         })
+    }, []);
+
+
+
+
+
+
     return (
         <div className='container mt-5' style={{ height: 550 }}>
             <div className='homepage-carousel-title'>
