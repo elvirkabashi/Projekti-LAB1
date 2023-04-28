59af231e216fab6d62c0979a9e4d9705f3bf7192
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2023-04-28 23:09:34 +0200

    search filter

--- backend/spring-boot-library/spring-boot-library/src/main/java/com/elibrary/springbootlibrary/dao/BookRepository.java
+++ backend/spring-boot-library/spring-boot-library/src/main/java/com/elibrary/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,14 @@
 package com.elibrary.springbootlibrary.dao;
 
 import com.elibrary.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title")String title, Pageable pageable);
+    Page<Book>findByCategory(@RequestParam("category")String category,Pageable pageable);
+
 }
