ddd23f4edd7c9b8ea1be489bb4f0595207f5de75
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2023-05-11 00:24:01 +0200

    Book Checkout

--- backend/spring-boot-library/src/main/java/com/elibrary/springbootlibrary/config/MyDataRestConfig.java
+++ backend/spring-boot-library/src/main/java/com/elibrary/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.elibrary.springbootlibrary.config;
 
 import com.elibrary.springbootlibrary.entity.Book;
+import com.elibrary.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -18,8 +19,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
         
         //Configure CORS Mapping
         cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
--- frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -4,12 +4,20 @@ import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
+import ReviewModel from "../../models/ReviewModel";
+import { LatestReviews } from "./LatestReviews";
 
 export const BookCheckoutPage = () => {
 
     const [book, setBook] = useState<BookModel>();
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
+
+
+    //review State
+    const [reviews, setReviews] = useState<ReviewModel[]>([]);
+    const [totalStars , setTotatStars] = useState(0);
+    const [isLoadingReview, setIsLoadingReview] = useState(true);
 
     const bookId = (window.location.pathname).split('/')[2];
 
@@ -46,7 +54,51 @@ export const BookCheckoutPage = () => {
     }, [bookId]);
 
 
-    if (isLoading) {
+    useEffect(() => {
+        const fetchBookReviews = async () => {
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+
+            const responseReviews = await fetch(reviewUrl);
+            if(!responseReviews.ok){
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJsonReviews = await responseReviews.json();
+
+            const responseData = responseJsonReviews._embedded.reviews;
+
+            const loadedReviews: ReviewModel[] = [];
+
+            let weightedStarReviews: number = 0;
+
+            for(const key in responseData){
+                loadedReviews.push({
+                    id: responseData[key].id,
+                    userEmail: responseData[key].userEmail,
+                    date: responseData[key].date,
+                    rating: responseData[key].rating,
+                    book_id: responseData[key].bookId,
+                    reviewDescription: responseData[key].reviewDescription,
+                })
+                weightedStarReviews = weightedStarReviews + responseData[key].rating;
+            }
+
+            if(loadedReviews){
+                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
+                setTotatStars(Number(round));
+            }
+
+            setReviews(loadedReviews);
+            setIsLoadingReview(false);
+        }
+
+        fetchBookReviews().catch((error: any) => {
+            setIsLoadingReview(false);
+            setHttpError(error.message);
+        })
+    }, [bookId]);
+    
+    if (isLoading || isLoadingReview) {
         return (
             <SpinnerLoading />
         )
@@ -77,12 +129,13 @@ export const BookCheckoutPage = () => {
                             <h2>{typeof book !== 'undefined' && book.title}</h2>
                             <h5 className='text-primary'>{typeof book !== 'undefined' && book.author}</h5>
                             <p className='lead'>{typeof book !== 'undefined' && book.description}</p>
-                            <StarsReview rating={4.5} size={32} />
+                            <StarsReview rating={totalStars} size={32} />
                         </div>
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false}/>
                 </div>
                 <hr />
+                <LatestReviews reviews={reviews} bookId={book?.id} mobile={false}/>
             </div>
             <div className='container d-lg-none mt-5'>
                 <div className='d-flex justify-content-center align-items-center'>
@@ -98,11 +151,12 @@ export const BookCheckoutPage = () => {
                         <h2>{typeof book !== 'undefined' && book.title}</h2>
                         <h5 className='text-primary'>{typeof book !== 'undefined' && book.author}</h5>
                         <p className='lead'>{typeof book !== 'undefined' && book.description}</p>
-                        <StarsReview rating={4.5} size={32} />
+                        <StarsReview rating={totalStars} size={32} />
                     </div>
                 </div>
                 <CheckoutAndReviewBox book={book} mobile = {true}/>
                 <hr />
+                <LatestReviews reviews={reviews} bookId={book?.id} mobile={true}/>
             </div>
         </div>
     );
