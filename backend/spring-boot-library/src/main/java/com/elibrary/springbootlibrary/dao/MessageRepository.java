7ea8d6a5bf781a8828c3f3e4e5e318547b069b2d
Blerta Jusufii <blertamicrosoft10@gmail.com>
2023-05-24 23:42:01 +0200

    Manage Admin Library

---
+++ backend/spring-boot-library/src/main/java/com/elibrary/springbootlibrary/dao/MessageRepository.java
@@ -0,0 +1,16 @@
+package com.elibrary.springbootlibrary.dao;
+
+import com.elibrary.springbootlibrary.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Pageable;
+
+
+
+
+public interface MessagesRepository extends JpaRepository<Message,Long> {
+
+    Page<Message> findByUserEmail (@RequestParam("user_email")String userEmail, Pageable pageable);
+    Page <Message> findByClosed(@RequestParam("closed")boolean closed, Pageable pageable);
+}
