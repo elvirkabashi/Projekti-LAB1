baef3b466d6f63d1cd1de2968299cb75f6553a46
Blerta Jusufii <blertamicrosoft10@gmail.com>
2023-06-04 00:06:12 +0200

    Increase, Decrease & Delete Book Fuctionality

--- frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ frontend/react-library/src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -17,6 +17,41 @@ export const ChangeQuantityOfBook:React.FC<{book:BookModel, deleteBook:any }>=(p
         fetchBookInState();
     },[]);
 
+    async function increaseQuantity(){
+        const url = `https://localhost:8443/api/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
+        const requestOptions ={
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if(!quantityUpdateResponse.ok){
+            throw new Error('Something went wrong!');
+        }
+        setQuantity(quantity + 1);
+        setRemaining(remaining + 1);
+    }
+
+    async function decreaseQuantity() {
+        const url = `https://localhost:8443/api/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
+        const requestOptions ={
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if(!quantityUpdateResponse.ok){
+            throw new Error('Something went wrong!');
+        }
+        setQuantity(quantity - 1);
+        setRemaining(remaining - 1);
+    }
+
+    
     async function deleteBook() {
         const url = `https://localhost:8443/api/admin/secure/delete/book/?bookId=${props.book?.id}`;
         const requestOptions ={
@@ -79,8 +114,8 @@ export const ChangeQuantityOfBook:React.FC<{book:BookModel, deleteBook:any }>=(p
                         <button className="m-1 btn btn-md btn-danger" onClick={deleteBook}>Delete</button>
                     </div>
                 </div>
-                <button className="m1 btn btn-md main-color text-white">Add Quantity</button>
-                <button className="m1 btn btn-md btn-warning">Decrease Quantity</button>
+                <button className="m1 btn btn-md main-color text-white" onClick={increaseQuantity}>Add Quantity</button>
+                <button className="m1 btn btn-md btn-warning" onClick={decreaseQuantity}>Decrease Quantity</button>
             </div>
         </div>
     );
